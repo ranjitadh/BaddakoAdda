77dee616fc666eccbb406ccea1cadbe54d3e3028
ranjit <ranzeet60@gmail.com>
2025-03-16 15:21:08 +0545

    Resolve ESLint errors and update next.config.js to use remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '', // Leave empty unless a specific port is required
-        pathname: '/**', // Allow all paths under this domain
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
